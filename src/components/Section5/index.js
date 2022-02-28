d1543a46f1d4052275ece0b2347680d98961c823
Nyamdorj <mungu0623@gmail.com>
2022-02-28 15:16:51 +0800

    section 4,5 mun zariin heseg duussan

--- src/components/Section5/index.js
+++ src/components/Section5/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ZarImg from "../../assets/images/zar1.png";
+import ZarImgSmall from "../../assets/images/zar_small.png";
 
 const Section5 = () => {
   return (
     <div>
-      <img className="bg-cover" src={ZarImg} />
+      <img className="bg-cover hidden lg:flex lg:visible" src={ZarImg} />
+      <img className="bg-cover  lg:hidden" src={ZarImgSmall} />
     </div>
   );
 };
